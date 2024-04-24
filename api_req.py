10a6b6dd54e6068b8f2fcb1ab55f0da7b635cbb5
Ann Connolly <biacatering4u@gmail.com>
2024-04-24 12:45:46 -0700

    venv

--- api_req.py
+++ api_req.py
@@ -5,7 +5,6 @@ def call_api():
     api_url = 'http://localhost:5071/api/Proposal/GetProposals'
 
     try:
-        # Sending a GET request to the API
         response = requests.get(api_url)
 
         # Check if the request was successful (status code 200)
@@ -18,15 +17,7 @@ def call_api():
             print('Failed to fetch data from API:', response.status_code)
             return None
     except Exception as e:
-        # If an exception occurs during the request, print an error message
         print('Error:', e)
         return None
 
-# Call the function to fetch data from the API
-# api_data = call_api()
 
-# # Check if data was fetched successfully
-# if api_data is not None:
-#     print('Data fetched successfully:', api_data)
-# else:
-#     print('Failed to fetch data from the API.')
